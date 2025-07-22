92807839009334084eb49aa879cb19982b5b5e65
Kishan Doshi <kishan.doshi88@gmail.com>
2025-07-22 12:55:32 +0300

    Project setup with nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	content: [
+		"./app/**/*.{js,jsx,ts,tsx}",
+		"./components/**/*.{js,jsx,ts,tsx}",
+	],
+	presets: [require("nativewind/preset")],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#123456",
+			},
+		},
+	},
+	plugins: [],
+};
